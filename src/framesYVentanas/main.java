9caf3cc6696731c30748b2b03cf40081042fa0a2
Miguel <bautistaortegam4@gmail.com>
2026-02-04 09:43:36 -0700

    primera ventan

---
+++ src/framesYVentanas/main.java
@@ -0,0 +1,10 @@
+package framesYVentanas;
+
+public class main {
+
+	public static void main(String[] args) {
+		// LOL
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module PrimeraaVentana {
+}
